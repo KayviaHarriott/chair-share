cf3ea632233c577fdbc6b3170fb39d65846c379e
Kayvia Harriott <55689354+KayviaHarriott@users.noreply.github.com>
2025-11-26 17:08:27 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import './App.css'
+import { Link, Route, Routes } from 'react-router'
+import { Home } from './pages/Home'
+import { About } from './pages/About'
+
+function App() {
+
+  return (
+    <>
+    <nav>
+        <Link to="/">Home</Link> | <Link to="/about">About</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
