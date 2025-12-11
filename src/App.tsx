6d7db0a652f3246bcee2795d31c404008a98f2f4
Kayvia Harriott <kayviaharriott@hotmail.com>
2025-12-11 18:38:29 -0500

    Added routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,31 @@ import { Updates } from './pages/Updates'
 import { NavBar } from './navigation/NavBar'
 import { Footer } from './navigation/Footer'
 
+// Admin pages
+import { AdminDashboardPage } from './pages/admin/AdminDashboardPage'
+import { BookMonitorPage } from './pages/admin/BookMonitorPage'
+import { CategoryManagementPage } from './pages/admin/CategoryManagementPage'
+import { MerchantApprovalPage } from './pages/admin/MerchantApprovalPage'
+
+// Auth pages
+import { LogInPage } from './pages/auth/LogInPage'
+import { RegisterPage } from './pages/auth/RegisterPage'
+import { ForgotPasswordPage } from './pages/auth/ForgotPasswordPage'
+
+// Client pages
+import { BrowsePage } from './pages/client/BrowsePage'
+import { MerchantDetailsPage } from './pages/client/MerchantDetailsPage'
+import { BookingRequestPage } from './pages/client/BookingRequestPage'
+
+// Merchant pages
+import { MerchantDashboard } from './pages/merchant/MerchantDashboard'
+import { MerchantProfilePage } from './pages/merchant/MerchantProfilePage'
+import { OnboardingPage } from './pages/merchant/OnboardingPage'
+
+// Errors
+import { NotFoundPage } from './pages/errors/NotFoundPage'
+
+
 function App() {
 
   return (
@@ -13,8 +38,33 @@ function App() {
       <NavBar/>
     </div>
       <Routes>
+        {/* Public routes */}
         <Route path="/" element={<Home />} />
         <Route path="/updates" element={<Updates />} />
+
+        {/* Auth */}
+        <Route path="/auth/login" element={<LogInPage />} />
+        <Route path="/auth/register" element={<RegisterPage />} />
+        <Route path="/auth/forgot-password" element={<ForgotPasswordPage />} />
+
+        {/* Client routes */}
+        <Route path="/browse" element={<BrowsePage />} />
+        <Route path="/merchant/:merchantId" element={<MerchantDetailsPage />} />
+        <Route path="/booking-request" element={<BookingRequestPage />} />
+
+        {/* Merchant routes */}
+        <Route path="/merchant/dashboard" element={<MerchantDashboard />} />
+        <Route path="/merchant/profile" element={<MerchantProfilePage />} />
+        <Route path="/merchant/onboarding" element={<OnboardingPage />} />
+
+        {/* Admin routes */}
+        <Route path="/admin/dashboard" element={<AdminDashboardPage />} />
+        <Route path="/admin/book-monitor" element={<BookMonitorPage />} />
+        <Route path="/admin/categories" element={<CategoryManagementPage />} />
+        <Route path="/admin/merchant-approvals" element={<MerchantApprovalPage />} />
+
+        {/* Fallback */}
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
       <div>
         <Footer/>
