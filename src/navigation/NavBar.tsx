d947afd0e53dd3438b3a75ec5991eb63f91d8431
Kayvia Harriott <kayviaharriott@hotmail.com>
2025-12-11 19:04:24 -0500

    Added url link for logo placement and updates links in nav bar

--- src/navigation/NavBar.tsx
+++ src/navigation/NavBar.tsx
@@ -4,18 +4,18 @@ export const NavBar = () => {
   return (
     <nav className="flex justify-center py-4 border-b border-gray-200 px-4 text-gray-500 bg-white">
       <div className="max-w-[1200px] w-full ">
-        <div className="w-full flex justify-between">
+        <div className="w-full flex justify-between items-center">
           <div className="w-1/3">
-            <p>ChairShare</p>
+            <Link to="/">ChairShare</Link>
           </div>
-          <div className="w-1/3 flex justify-between gap-4">
+          <div className="w-1/3 flex items-center justify-between gap-4">
             <Link to="/">Services</Link>
             <Link to="/about">How it Works</Link>
             <Link to="/merchants">Merchants</Link>
           </div>
-          <div className="w-1/3 flex justify-end gap-3">
+          <div className="w-1/3 flex justify-end items-center gap-3">
             <Link to="/merchants">Sign In</Link>
-            <Link to="/merchants">Get Started</Link>
+              <Link className="bg-amber-500 px-6 py-2 text-white rounded-full" to="/merchants">Get Started</Link>
           </div>
         </div>
       </div>
