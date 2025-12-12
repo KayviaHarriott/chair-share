9d0cc84c1355fcf896120e0d1d593da5467aad43
Kayvia Harriott <kayviaharriott@hotmail.com>
2025-12-11 20:14:30 -0500

    Added logo

--- src/navigation/NavBar.tsx
+++ src/navigation/NavBar.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 // import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
-import { KeyboardArrowRight } from "@mui/icons-material";
+// import { KeyboardArrowRight } from "@mui/icons-material"; 
 
 export const NavBar = () => {
   return (
@@ -15,12 +15,13 @@ export const NavBar = () => {
            {/* <KeyboardArrowRight/>  */}
          </div>
         </div>
-      <nav className="flex justify-center py-4 border-b border-gray-200 px-4 text-gray-500 bg-white">
+      <nav className="flex justify-center py-4 border-b border-gray-200 px-4 text-gray-800 bg-white">
         
         <div className="max-w-[1200px] w-full ">
           <div className="w-full flex justify-between items-center">
-            <div className="w-1/3">
-              <Link to="/">ChairShare</Link>
+            <div className="w-1/3 flex items-center gap-2">
+              {/* <Link to="/"><img className="h-[50px]" src="./imgs/Logo.png" /></Link> */}
+              <Link className="text-xl" to="/"><p>ChairShare</p></Link>
             </div>
             <div className="w-1/3 flex items-center justify-center gap-4">
               <Link to="/">Services</Link>
