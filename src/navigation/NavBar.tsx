78bee71d55fc5e5979619f7f3175a276fd70d173
KayviaHarriott <genspark_dev@genspark.ai>
2025-12-14 00:47:16 +0000

    fix(navbar): add relative positioning to keep dropdown open on hover

--- src/navigation/NavBar.tsx
+++ src/navigation/NavBar.tsx
@@ -136,85 +136,87 @@ export const NavBar = () => {
           <p className="text-sm">FAQs</p>
         </div>
       </div>
-      <nav className="shadow-sm/5 bg-white border-b border-gray-200 flex flex-col justify-center pt-4 items-center ">
-        <div className="max-w-[1200px] px-4 pb-2 w-full">
-          <div className="w-full flex justify-between items-center">
-            <div className="w-1/3 flex items-center gap-2 mb-1">
-              <Link to="/">
-                <img className="h-[50px]" src="./imgs/HorizontalLogo.png" />
-              </Link>
-            </div>
-            <div className="w-1/3 flex items-center justify-center gap-6">
-              <Link className="hover:text-amber-600 cursor-pointer" to="/about">How it Works</Link>
-              <Link className="hover:text-amber-600 cursor-pointer" to="/merchants">Services</Link>
-              <Link className="hover:text-amber-600 cursor-pointer" to="/">Near Me</Link>
-            </div>
-            <div className="w-1/3 flex justify-end items-center gap-6 ">
-              <Link className="hover:text-amber-600" to="/merchants">Sign In</Link>
-              <Link
-                className="bg-linear-to-br from-amber-500 to-[#BF4E30] px-6 py-2 text-white rounded-full"
-                to="/register"
-              >
-                Get Started
-              </Link>
-            </div>
-          </div>
-        </div>
-
-        <div className="border-t w-full pt-1 border-gray-200 flex items-center justify-center">
-          <div className="max-w-[1200px] w-full flex gap-8 pb-4 px-4 ">
-            {categories.map((category) => (
-              <div
-                key={category.name}
-                className="relative"
-                onMouseEnter={() => setHoveredCategory(category.name)}
-                onMouseLeave={() => setHoveredCategory(null)}
-              >
-                <button className="hover:text-amber-600 transition-colors flex items-center mt-3">
-                  {category.name}
-
-                  {/* Only show arrow if subcategories exist */}
-                  {category.subcategories && (
-                    <KeyboardArrowDownRounded sx={{ marginTop: "0px" }} />
-                  )}
-                </button>
+      <div className="relative" onMouseLeave={() => setHoveredCategory(null)}>
+        <nav className="shadow-sm/5 bg-white border-b border-gray-200 flex flex-col justify-center pt-4 items-center ">
+          <div className="max-w-[1200px] px-4 pb-2 w-full">
+            <div className="w-full flex justify-between items-center">
+              <div className="w-1/3 flex items-center gap-2 mb-1">
+                <Link to="/">
+                  <img className="h-[50px]" src="./imgs/HorizontalLogo.png" />
+                </Link>
               </div>
-            ))}
+              <div className="w-1/3 flex items-center justify-center gap-6">
+                <Link className="hover:text-amber-600 cursor-pointer" to="/about">How it Works</Link>
+                <Link className="hover:text-amber-600 cursor-pointer" to="/merchants">Services</Link>
+                <Link className="hover:text-amber-600 cursor-pointer" to="/">Near Me</Link>
+              </div>
+              <div className="w-1/3 flex justify-end items-center gap-6 ">
+                <Link className="hover:text-amber-600" to="/merchants">Sign In</Link>
+                <Link
+                  className="bg-linear-to-br from-amber-500 to-[#BF4E30] px-6 py-2 text-white rounded-full"
+                  to="/register"
+                >
+                  Get Started
+                </Link>
+              </div>
+            </div>
           </div>
-        </div>
-      </nav>
-      <div className="">
-        <div className="">
-          {categories.map(
-            (category) =>
-              hoveredCategory === category.name &&
-              category.subcategories && ( // ⬅ prevents dropdown from rendering if none
+
+          <div className="border-t w-full pt-1 border-gray-200 flex items-center justify-center">
+            <div className="max-w-[1200px] w-full flex gap-8 pb-4 px-4 ">
+              {categories.map((category) => (
                 <div
                   key={category.name}
-                  className="absolute top-full left-0 w-full bg-white border-gray-200 px-6 pt-5 pb-8 z-50 flex justify-center"
+                  className="relative"
+                  onMouseEnter={() => setHoveredCategory(category.name)}
                 >
-                  <div className="max-w-[1200px] w-full px-4">
-                    <div className="flex gap-24">
-                      {category.subcategories.map((subcategory, index) => (
-                        <div key={index}>
-                          <p className="pb-2 font-bold hover:text-amber-600 cursor-pointer">{subcategory.title}</p>
-                          <ul className="space-y-1 text-gray-800">
-                            {subcategory.items.map((item, itemIndex) => (
-                              <li
-                                key={itemIndex}
-                                className="hover:text-amber-600 cursor-pointer"
-                              >
-                                {item}
-                              </li>
-                            ))}
-                          </ul>
-                        </div>
-                      ))}
+                  <button className="hover:text-amber-600 transition-colors flex items-center mt-3">
+                    {category.name}
+
+                    {/* Only show arrow if subcategories exist */}
+                    {category.subcategories && (
+                      <KeyboardArrowDownRounded sx={{ marginTop: "0px" }} />
+                    )}
+                  </button>
+                </div>
+              ))}
+            </div>
+          </div>
+        </nav>
+        <div className="">
+          <div className="">
+            {categories.map(
+              (category) =>
+                hoveredCategory === category.name &&
+                category.subcategories && ( // ⬅ prevents dropdown from rendering if none
+                  <div
+                    key={category.name}
+                    className="absolute top-full left-0 w-full bg-white border-gray-200 px-6 pt-5 pb-8 z-50 flex justify-center"
+                    onMouseEnter={() => setHoveredCategory(category.name)}
+                  >
+                    <div className="max-w-[1200px] w-full px-4">
+                      <div className="flex gap-24">
+                        {category.subcategories.map((subcategory, index) => (
+                          <div key={index}>
+                            <p className="pb-2 font-bold hover:text-amber-600 cursor-pointer">{subcategory.title}</p>
+                            <ul className="space-y-1 text-gray-800">
+                              {subcategory.items.map((item, itemIndex) => (
+                                <li
+                                  key={itemIndex}
+                                  className="hover:text-amber-600 cursor-pointer"
+                                >
+                                  {item}
+                                </li>
+                              ))}
+                            </ul>
+                          </div>
+                        ))}
+                      </div>
                     </div>
                   </div>
-                </div>
-              )
-          )}
+                )
+            )}
+          </div>
         </div>
       </div>
     </div>
